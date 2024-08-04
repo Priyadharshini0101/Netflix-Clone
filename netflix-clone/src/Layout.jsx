5ed72e21c95bc62c0a1c7681340d3d19d7715603
Priyadharshini S <priyadharshinis5102@gmail.com>
2024-08-04 13:13:01 +0530

    Added Changes

--- netflix-clone/src/Layout.jsx
+++ netflix-clone/src/Layout.jsx
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Header, Footer } from "./components/index.js";
 
-
 function Layout() {
-  
 
-  
   return (
     <>
       <Header></Header>
